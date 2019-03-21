541413e89ba0427e36618ffd51d597e572134a4c
anlaneg <anlaneg@126.com>
2019-03-21 10:05:07 +0800

    init example project

---
+++ cmake-project/example-project/src/main/a.c
@@ -0,0 +1,6 @@
+#include "a.h"
+double get_sqrt(double var1)
+{
+    return sqrt(var1);
+}
+
---
+++ cmake-project/example-project/src/main/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "a.h"
+int main()
+{
+	double b=25.0;
+	double a=0.0;
+	a=get_sqrt(b);
+
+	printf("a is %lf, b is %lf\n",a,b);
+	return 0;
+}
+
+
+
