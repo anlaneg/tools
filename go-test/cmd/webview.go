cf0f22743c24aa995f84b2378adcb200fa74f1c7
anlaneg <anlaneg@126.com>
2023-05-13 16:37:20 +0800

    tools: add options

--- go-test/cmd/webview.go
+++ go-test/cmd/webview.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/polevpn/webview"
+import (
+	"fmt"
+	"github.com/jessevdk/go-flags"
+	"github.com/polevpn/webview"
+	"os"
+)
+
+type Option struct {
+	Url string `short:"u" long:"url" required:"false" default:"https://fanyi.baidu.com/#en/zh/ABC" description:"URL to be accessed"`
+}
 
 func main() {
+	var options Option
+	if _, err := flags.ParseArgs(&options, os.Args); err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
 
 	w := webview.New(800, 600, false, true)
 	defer w.Destroy()
 	w.SetTitle("Minimal webview example")
 	w.SetSize(800, 600, webview.HintNone)
-	w.Navigate("https://www.baidu.com")
+
+	//fmt.Println(options.Url)
+	w.Navigate(options.Url)
 	w.Run()
 }
--- go-test/go.mod
+++ go-test/go.mod
@@ -2,4 +2,8 @@ module github.com/anlaneg/tools/go-test
 
 go 1.19
 
-require github.com/polevpn/webview v1.0.8 // indirect
+require (
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	github.com/polevpn/webview v1.0.8 // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
+)
